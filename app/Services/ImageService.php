3a95f620050a9f6b4bfb9514abdee5b7e481b5ec
鈴木新之介 <shin.suzuki0605@gmail.com>
2024-11-20 19:09:34 +0900

    ImageService修正

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -3,44 +3,53 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Storage;
-use InterventionImage;
-
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImageService
 {
-//画像とフォルダ名が渡される
-  public static function upload($imageFile, $folderName){
-    //dd($imageFile['image']);
-    if(is_array($imageFile))
+    // 画像とフォルダ名が渡される
+    public static function upload($imageFile, $folderName)
     {
-      $file = $imageFile['image'];
-    } else {
-      $file = $imageFile;
+        // ファイルを判別
+        $file = is_array($imageFile) ? $imageFile['image'] : $imageFile;
+
+        // ファイル名を生成
+        $fileName = uniqid(rand() . '_');
+        $extension = $file->extension();
+        $fileNameToStore = $fileName . '.' . $extension;
+
+        // ImageManagerを利用して画像をリサイズ
+        $manager = new ImageManager(new Driver());
+        $resizedImage = $manager->read($file->getRealPath())
+                                ->resize(200, 200)
+                                ->encode();
+
+        // リサイズ後の画像を保存
+        Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
+
+        return $fileNameToStore;
     }
 
-    $fileName = uniqid(rand().'_');
-    $extension = $file->extension();
-    $fileNameToStore = $fileName. '.' . $extension;
-    $resizedImage = InterventionImage::make($file)->resize(200, 200)->encode();
-    Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage );
-
-    return $fileNameToStore;
-  }
-  public static function productUpload($imageFile, $folderName){
-    //dd($imageFile['image']);
-    if(is_array($imageFile))
+    public static function productUpload($imageFile, $folderName)
     {
-      $file = $imageFile['image'];
-    } else {
-      $file = $imageFile;
+        // ファイルを判別
+        $file = is_array($imageFile) ? $imageFile['image'] : $imageFile;
+
+        // ファイル名を生成
+        $fileName = uniqid(rand() . '_');
+        $extension = $file->extension();
+        $fileNameToStore = $fileName . '.' . $extension;
+
+        // ImageManagerを利用して画像をリサイズ
+        $manager = new ImageManager(new Driver());
+        $resizedImage = $manager->read($file->getRealPath())
+                                ->resize(300, 300)
+                                ->encode();
+
+        // リサイズ後の画像を保存
+        Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage);
+
+        return $fileNameToStore;
     }
-
-    $fileName = uniqid(rand().'_');
-    $extension = $file->extension();
-    $fileNameToStore = $fileName. '.' . $extension;
-    $resizedImage = InterventionImage::make($file)->resize(300, 300)->encode();
-    Storage::put('public/' . $folderName . '/' . $fileNameToStore, $resizedImage );
-
-    return $fileNameToStore;
-  }
 }
