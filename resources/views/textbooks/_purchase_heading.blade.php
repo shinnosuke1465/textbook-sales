6997e22310eac8299ae38a9830a8bbe6be65275c
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-01-05 23:08:06 +0900

    修正

--- resources/views/textbooks/_purchase_heading.blade.php
+++ resources/views/textbooks/_purchase_heading.blade.php
@@ -1,4 +1,4 @@
-<div class="font-weight-bold text-center pb-3 pt-3" style="font-size: 24px">{{ $item->name }}</div>
+<div class="font-weight-bold text-center" style="font-size: 24px">{{ $item->name }}</div>
 
 <div class="row">
     <div class="col-4 offset-1">
