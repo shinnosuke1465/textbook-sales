8d56dcf3f5e9d6a8f53bbffbe00d3113d4c4b978
鈴木新之介 <shin.suzuki0605@gmail.com>
2025-01-08 22:18:31 +0900

    フロントコーディング

--- resources/views/textbooks/_detail.blade.php
+++ resources/views/textbooks/_detail.blade.php
@@ -1,9 +1,14 @@
 <div class="pg-textbook-detail__content">
     {{-- 商品画像 --}}
     <div class="pg-textbook-detail__image">
+        <div class="pg-textbook-detail__button-back">
+            @include('components.button_back', [
+                'href' => route('textbooks.index'),
+            ])
+        </div>
         @include('components.thumbnail', [
-        'type' => 'textbooks',
-        'filename' => $item->image_file_name,
+            'type' => 'textbooks',
+            'filename' => $item->image_file_name,
         ])
     </div>
 
@@ -11,28 +16,31 @@
         <p class="pg-textbook-detail__name">
             {{ $item->name }}
         </p>
-
-        <p class="pg-textbook-detail__description">
-            {{ $item->description }}
-        </p>
-
         <p class="pg-textbook-detail__university">
             {{ $item->university->name }} / {{ $item->faculty->name }}
         </p>
 
-        <p class="pg-textbook-detail__condition">
-            {{ $item->condition->name }}
+        <div class="pg-textbook-detail__price">
+            <i class="fas fa-yen-sign"></i>
+            <span>{{ number_format($item->price) }}</span>
+        </div>
+
+
+        <div class="pg-textbook-detail__description-area">
+            <p class="pg-textbook-detail__description-title">商品の説明</p>
+            <p class="pg-textbook-detail__description-text">{{ $item->description }}</p>
+        </div>
+
+
+        <p class="pg-textbook-detail__description-area">
+        <p class="pg-textbook-detail__description-title">商品の状態</p>
+        <p class="pg-textbook-detail__description-text">{{ $item->condition->name }}</p>
         </p>
 
-        <p class="pg-textbook-detail__price">
-            {{ number_format($item->price) }}
-        </p>
-
-        <p class="pg-textbook-detail__created">
-            {{ $item->created_at->format('Y年n月j日 H:i') }}
-        </p>
-
-        <a href="{{route('textbook.purchase', [$item->id])}}">購入</a>
-
+        @if ($item->isStateSelling)
+            <a class="pg-textbook-detail__button" href="{{ route('textbook.purchase', [$item->id]) }}">購入</a>
+        @else
+            <button class="pg-textbook-detail__button-sold" disabled>売却済み</button>
+        @endif
     </div>
 </div>
--- resources/views/textbooks/_list_item.blade.php
+++ resources/views/textbooks/_list_item.blade.php
@@ -2,11 +2,11 @@
     <a class="c-textbook-list-item__anchor" href="{{ route('textbooks.show', [$textbook->id]) }}">
         {{-- 商品画像 --}}
         <div class="c-textbook-list-item__image-area">
-            <div class="c-textbook-list-item__image-area">
-            @include('components.thumbnail', [
-            'type' => 'textbooks',
-            'filename' => $textbook->image_file_name,
-            ])
+            <div>
+                @include('components.thumbnail', [
+                'type' => 'textbooks',
+                'filename' => $textbook->image_file_name,
+                ])
             </div>
             <div class="c-textbook-list-item__price">
                 <i class="fas fa-yen-sign"></i>
